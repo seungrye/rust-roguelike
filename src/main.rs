136f7845d2b2c1174ea8fbcc549ddf50b49c4b32
안 승례 <seungrye@gmail.com>
2024-01-01 17:11:15 +0900

    codes for `Chapter 2 - Entities and Components`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,64 @@
-use rltk::{Rltk, GameState};
+use rltk::{Rltk, GameState, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
 
-struct State {}
+struct State {
+    ecs: World
+}
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftMover{};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
+        self.run_systems();
+        player_input(self, ctx);
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
+}
+
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+#[derive(Component)]
+struct LeftMover {
+
+}
+impl<'a> System<'a> for LeftMover {
+    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        for(_lefty, pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x <0 {
+                pos.x = 79
+            }
+        }
+    }
+}
+
+#[derive(Component, Debug)]
+struct Player {
+
 }
 
 fn main() -> rltk::BError {
@@ -13,6 +66,57 @@ fn main() -> rltk::BError {
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
-    let gs = State{ };
+    
+    let mut gs = State{ ecs: World::new() };
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
+
+    gs.ecs.create_entity()
+    .with(Position{x:40, y:35})
+    .with(Renderable{
+        glyph: rltk::to_cp437('@'),
+        fg: RGB::named(rltk::YELLOW),
+        bg: RGB::named(rltk::BLACK)
+    })
+    .with(Player{})
+    .build();
+
+    for i in 0..10 {
+        gs.ecs.create_entity()
+        .with(Position{x:i*7, y:20})
+        .with(Renderable{
+            glyph: rltk::to_cp437('☺'),
+            fg: RGB::named(rltk::RED),
+            bg: RGB::named(rltk::BLACK)    
+        })
+        .with(LeftMover{})
+        .build();
+    }
+
     rltk::main_loop(context, gs)
+}
+
+fn player_input(gs:&mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {},
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        }
+    }
+}
+
+fn try_move_player(delta_x:i32, delta_y: i32, ecs:&mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut player = ecs.write_storage::<Player>();
+    // 만약 Position과 Player를 모두 가진 엔티티가 있다면, for 구문을 실행합니다.
+    for (_player, pos) in (&mut player, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
 }
