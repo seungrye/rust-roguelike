9866d0ff9ef2a012ce198f06a16bc8d9a6acb24c
안승례 <seungrye@devguru.co.kr>
2024-02-20 04:38:48 +0900

    remove magic number

--- src/map.rs
+++ src/map.rs
@@ -21,7 +21,7 @@ impl Map {
     fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
         for y in cmp::min(y1, y2)..=cmp::max(y1, y2) {
             let idx = self.xy_idx(x, y);
-            if idx > 0 && idx < 80 * 50 {
+            if idx > 0 && idx < (self.width * self.height) as usize {
                 self.tiles[idx] = TileType::Floor;
             }
         }
@@ -30,14 +30,14 @@ impl Map {
     fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
         for x in cmp::min(x1, x2)..=cmp::max(x1, x2) {
             let idx = self.xy_idx(x, y);
-            if idx > 0 && idx < 80 * 50 {
+            if idx > 0 && idx < (self.width * self.height) as usize {
                 self.tiles[idx] = TileType::Floor;
             }
         }
     }
 
     pub fn xy_idx(&self, x: i32, y: i32) -> usize {
-        (y as usize * 80) + x as usize
+        (y * self.width) as usize + x as usize
     }
 
     pub fn new_map_rooms_and_corridors() -> Map {
@@ -57,8 +57,8 @@ impl Map {
         for _ in 0..MAX_ROOMS {
             let w = rng.range(MIN_SIZE, MAX_SIZE);
             let h = rng.range(MIN_SIZE, MAX_SIZE);
-            let x = rng.roll_dice(1, 80 - w - 1) - 1;
-            let y = rng.roll_dice(1, 50 - h - 1) - 1;
+            let x = rng.roll_dice(1, map.width - w - 1) - 1;
+            let y = rng.roll_dice(1, map.height - h - 1) - 1;
             let new_room = Rect::new(x, y, w, h);
             let mut ok = true;
             for other_room in map.rooms.iter() {
